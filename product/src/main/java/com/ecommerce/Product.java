ca53a606a2310a739653247db116ad1e0b2f2bf9
AhmedHm <65871345+ahmedhesham766@users.noreply.github.com>
2025-02-05 01:16:02 +0200

    add EurekaServer and add product and cart  instances to it.

--- product/src/main/java/com/ecommerce/Product.java
+++ product/src/main/java/com/ecommerce/Product.java
@@ -2,10 +2,11 @@ package com.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Product {
     public static void main(String[] args) {
         SpringApplication.run(Product.class, args);
